df1b7ca39dbe0cf56538e32d51c0770877bef870
imaginingforward <thusoozooni@gmail.com>
2025-08-16 03:38:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import SubmissionResultPage from "./pages/SubmissionResultPage";
-import SpaceForm from "./components/SpaceForm";
+import MatchingPage from "./pages/MatchingPage";
 import ImFoIntelligencePage from "./pages/ImFoIntelligencePage";
 
 const queryClient = new QueryClient();
@@ -16,13 +17,17 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/rfp-match">
+      <BrowserRouter>
         <Routes>
+          {/* Search Engine Routes */}
           <Route path="/" element={<Index />} />
-          {/* <Route path="/standard-form" element={<SpaceForm />} /> */}
-          <Route path="/submission-result" element={<SubmissionResultPage />} />
           <Route path="/intelligence" element={<ImFoIntelligencePage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
+          {/* RFP Match Tool Routes */}
+          <Route path="/match" element={<MatchingPage />} />
+          <Route path="/submission-result" element={<SubmissionResultPage />} />
+          
+          {/* Catch-all not found */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
